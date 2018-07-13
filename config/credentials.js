5dc40c0282341398b3dc30c6d5568001631329d8
pradeepch <chpradeep@cdac.in>
2018-07-13 04:17:23 +0000

    With mqtt settings

--- config/credentials.js
+++ config/credentials.js
@@ -1,5 +1,12 @@
 module.exports = {
     // mongoDB database to be connected
     //database: 'mongodb://localhost:27017/cdaciot'
-    database: 'mongodb://admin:admin123@ds147390.mlab.com:47390/platform'
+    database: 'mongodb://admin:admin123@ds147390.mlab.com:47390/platform',
+    mqttSettings : {
+        BROKER_URL : "iot.eclipse.org",
+        PUB_TOPIC : "action",
+        SUB_TOPIC : "event",
+        USERNAME : "username",
+        PASSWORD : "password"
+    }
 }
